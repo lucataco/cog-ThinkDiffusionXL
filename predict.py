585eed60072e6e0c1b20dc9ee80350151aa65fb2
Luis C <me@lucataco.com>
2023-11-06 18:04:06 -0500

    Added NSFW checker

--- predict.py
+++ predict.py
@@ -2,7 +2,10 @@ from cog import BasePredictor, Input, Path
 import os
 import time
 import torch
+import subprocess
+import numpy as np
 from typing import List
+from transformers import CLIPImageProcessor
 from diffusers import (
     DDIMScheduler, 
     DiffusionPipeline,
@@ -10,13 +13,19 @@ from diffusers import (
     EulerAncestralDiscreteScheduler, 
     EulerDiscreteScheduler,
     HeunDiscreteScheduler, 
-    PNDMScheduler
+    PNDMScheduler,
+)
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker,
 )
 
 MODEL_NAME = "ThinkDiffusion/ThinkDiffusionXL"
 SDXL_REFINER = "stabilityai/stable-diffusion-xl-refiner-1.0"
 SDXL_MODEL_CACHE = "sdxl-cache/"
 REFINER_MODEL_CACHE = "refiner-cache/"
+SAFETY_CACHE = "./safety-cache"
+FEATURE_EXTRACTOR = "./feature-extractor"
+SAFETY_URL = "https://weights.replicate.delivery/default/sdxl/safety-1.0.tar"
 
 SCHEDULERS = {
     "DDIM": DDIMScheduler,
@@ -27,10 +36,24 @@ SCHEDULERS = {
     "PNDM": PNDMScheduler,
 }
 
+def download_weights(url, dest):
+    start = time.time()
+    print("downloading url: ", url)
+    print("downloading to: ", dest)
+    subprocess.check_call(["pget", "-x", url, dest], close_fds=False)
+    print("downloading took: ", time.time() - start)
+
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         t1 = time.time()
+        print("Loading safety checker...")
+        if not os.path.exists(SAFETY_CACHE):
+            download_weights(SAFETY_URL, SAFETY_CACHE)
+        self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
+            SAFETY_CACHE, torch_dtype=torch.float16
+        ).to("cuda")
+        self.feature_extractor = CLIPImageProcessor.from_pretrained(FEATURE_EXTRACTOR)
         print("Loading sdxl txt2img pipeline...")
         self.txt2img_pipe = DiffusionPipeline.from_pretrained(
             MODEL_NAME,
@@ -42,7 +65,17 @@ class Predictor(BasePredictor):
         t2 = time.time()
         print("Setup sdxl & refiner took: ", t2 - t1)
 
-
+    def run_safety_checker(self, image):
+        safety_checker_input = self.feature_extractor(image, return_tensors="pt").to(
+            "cuda"
+        )
+        np_image = [np.array(val) for val in image]
+        image, has_nsfw_concept = self.safety_checker(
+            images=np_image,
+            clip_input=safety_checker_input.pixel_values.to(torch.float16),
+        )
+        return image, has_nsfw_concept
+    
     @torch.inference_mode()
     def predict(
         self,
@@ -71,7 +104,7 @@ class Predictor(BasePredictor):
         scheduler: str = Input(
             description="scheduler",
             choices=SCHEDULERS.keys(),
-            default="DDIM",
+            default="DPMSolverMultistep",
         ),
         num_inference_steps: int = Input(
             description="Number of denoising steps", ge=1, le=500, default=30
@@ -81,6 +114,9 @@ class Predictor(BasePredictor):
         ),
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
+        ),
+        nsfw_checker: bool = Input(
+            description="Select to filter for NSFW content", default=True
         ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
@@ -104,8 +140,13 @@ class Predictor(BasePredictor):
         output = pipe(**common_args, **sdxl_kwargs)
         pipe.scheduler = SCHEDULERS[scheduler].from_config(pipe.scheduler.config)
 
+        _, has_nsfw_content = self.run_safety_checker(output.images)
+
         output_paths = []
-        for i, _ in enumerate(output.images):
+        for i, nsfw in enumerate(has_nsfw_content):
+            if nsfw_checker and nsfw:
+                print(f"NSFW content detected in image {i}")
+                continue
             output_path = f"/tmp/out-{i}.png"
             output.images[i].save(output_path)
             output_paths.append(Path(output_path))
